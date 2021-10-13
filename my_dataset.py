29cff2b2c2d19ca1508710329dae7154f9f83ecf
lee linfeng <2033211717@qq.com>
2021-10-13 17:10:12 +0800

    refactor:my_dataset.py

--- my_dataset.py
+++ my_dataset.py
@@ -8,62 +8,59 @@ from lxml import etree
 
 
 class VOCDataSet(Dataset):
-    """读取解析PASCAL VOC2007/2012数据集"""
+    def __init__(self, voc_root: str,
+                 year: str = '2007',
+                 transforms=None,
+                 train_set: str = 'train.txt'):
+        assert year in ['2007', '2012'], 'year must be in ["2007","2012"]'
 
-    def __init__(self, voc_root, year="2012", transforms=None, train_set='train.txt'):
-        assert year in ["2007", "2012"], "year must be in ['2007', '2012']"
+        self.root = os.path.join(voc_root, 'VOCdevkit', f'VOC{year}')
+        self.img_root = os.path.join(self.root, 'JPEGImages')
+        self.annotations_root = os.path.join(self.root, 'Annotations')
 
-        self.root = os.path.join(voc_root, "VOCdevkit", f"VOC{year}")
-        self.img_root = os.path.join(self.root, "JPEGImages")
-        self.annotations_root = os.path.join(self.root, "Annotations")
+        # train.txt path
+        txt_list = os.path.join(self.root, 'ImageSets', 'Main', train_set)
 
-        txt_list = os.path.join(self.root, "ImageSets", "Main", train_set)
-
-        with open(txt_list) as read:
-            self.xml_list = [os.path.join(self.annotations_root, line.strip() + ".xml")
+        # read train.txt content and get the xml file path
+        with open(txt_list, 'r') as read:
+            self.xml_list = [os.path.join(self.annotations_root, line.strip() + '.xml')
                              for line in read.readlines() if len(line.strip()) > 0]
 
-        # read class_indict
-        json_file = "my_voc_classes.json"
-        assert os.path.exists(json_file), "{} file not exist.".format(json_file)
-        json_file = open(json_file, 'r')
-        self.class_dict = json.load(json_file)
-        json_file.close()
+        json_file = 'my_voc_classes.json'
+        assert os.path.exists(json_file) is True, 'json_file is not exist'
+        with open(json_file, 'r') as f:
+            self.class_dict = json.load(f)  # class_dict={class1:1,class2:2}
 
         self.transforms = transforms
 
     def __len__(self):
         return len(self.xml_list)
 
-    def __getitem__(self, idx):
-        # read xml
+    def __getitem__(self, idx: int):
         xml_path = self.xml_list[idx]
-        with open(xml_path) as fid:
-            xml_str = fid.read()
-        xml = etree.fromstring(xml_str)
-        data = self.parse_xml_to_dict(xml)["annotation"]
-        data_height = int(data["size"]["height"])
-        data_width = int(data["size"]["width"])
-        height_width = [data_height, data_width]
+        with open(xml_path, 'r') as f:
+            xml_str = f.read()
 
-        # img_path = os.path.join(self.img_root, data["filename"])
+        xml = etree.fromstring(xml_str)  # Returns the root node
+        data = self.parse_xml_to_dict(xml)
+        annotation = data['annotation']
+        img_height = int(annotation['size']['height'])
+        img_width = int(annotation['size']['width'])
+        height_width = [img_height, img_width]
 
         img_path = xml_path.replace('Annotations', 'JPEGImages').replace('xml', 'jpg')
         image = Image.open(img_path)
 
-        # if image.format != "JPEG":
-        #         raise ValueError("Image '{}' format not JPEG".format(img_path))
-
-        assert "object" in data, "{} lack of object information.".format(xml_path)
+        assert 'object' in annotation, '{} lack of object informations'.format(xml_path)
         boxes = []
         labels = []
         iscrowd = []
-        for obj in data["object"]:
+        for obj in annotation['object']:
             # 将所有的gt box信息转换成相对值0-1之间
-            xmin = float(obj["bndbox"]["xmin"]) / data_width
-            xmax = float(obj["bndbox"]["xmax"]) / data_width
-            ymin = float(obj["bndbox"]["ymin"]) / data_height
-            ymax = float(obj["bndbox"]["ymax"]) / data_height
+            xmin = float(obj['bndbox']['xmin']) / img_width
+            xmax = float(obj['bndbox']['xmax']) / img_width
+            ymin = float(obj['bndbox']['ymin']) / img_height
+            ymax = float(obj['bndbox']['ymax']) / img_height
 
             # 进一步检查数据，有的标注信息中可能有w或h为0的情况，这样的数据会导致计算回归loss为nan
             if xmax <= xmin or ymax <= ymin:
@@ -71,68 +68,69 @@ class VOCDataSet(Dataset):
                 continue
 
             boxes.append([xmin, ymin, xmax, ymax])
-            labels.append(self.class_dict[obj["name"]])
-            if "difficult" in obj:
-                iscrowd.append(int(obj["difficult"]))
+            labels.append(self.class_dict[obj['name']])
+
+            if 'difficult' in obj:
+                iscrowd.append(int(obj['difficult']))
             else:
                 iscrowd.append(0)
 
-        # convert everything into a torch.Tensor
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
         labels = torch.as_tensor(labels, dtype=torch.int64)
         iscrowd = torch.as_tensor(iscrowd, dtype=torch.int64)
         height_width = torch.as_tensor(height_width, dtype=torch.int64)
         image_id = torch.tensor([idx])
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])  # image area
 
         target = {}
-        target["boxes"] = boxes
-        target["labels"] = labels
-        target["image_id"] = image_id
-        target["area"] = area
-        target["iscrowd"] = iscrowd
-        target["height_width"] = height_width
-
+        target['boxes'] = boxes
+        target['labels'] = labels
+        target['image_id'] = image_id
+        target['area'] = area
+        target[height_width] = iscrowd
+        target['height_width'] = height_width
         if self.transforms is not None:
             image, target = self.transforms(image, target)
-
         return image, target
 
     def get_height_and_width(self, idx):
-        # read xml
         xml_path = self.xml_list[idx]
-        with open(xml_path) as fid:
-            xml_str = fid.read()
+
+        with open(xml_path, 'r') as f:
+            xml_str = f.read()
+
         xml = etree.fromstring(xml_str)
-        data = self.parse_xml_to_dict(xml)["annotation"]
-        data_height = int(data["size"]["height"])
-        data_width = int(data["size"]["width"])
-        return data_height, data_width
+        data = self.parse_xml_to_dict(xml)
+        annotation = data['annotation']
+        height = int(annotation['size']['height'])
+        width = int(annotation['size']['width'])
+        return height, width
 
-    def parse_xml_to_dict(self, xml):
+    def parse_xml_to_dict(self, xml) -> dict:
         """
-        将xml文件解析成字典形式，参考tensorflow的recursive_parse_xml_to_dict
-        Args：
-            xml: xml tree obtained by parsing XML file contents using lxml.etree
-        Returns:
-            Python dictionary holding XML contents.
+        docs:
+            将xml文件解析成字典形式
+            <name>bob<name>
+            xml.tag:node name eg:<name>
+            xml.text:node content eg:bob
+        :param xml:
+        :return:{node.tag:node.content} eg{'size':{'w':'1','h':'2','d':'3'}}
         """
-
-        if len(xml) == 0:  # 遍历到底层，直接返回tag对应的信息
+        if len(xml) == 0:
             return {xml.tag: xml.text}
 
         result = {}
         for child in xml:
-            child_result = self.parse_xml_to_dict(child)  # 递归遍历标签信息
+            child_result = self.parse_xml_to_dict(child)
             if child.tag != 'object':
                 result[child.tag] = child_result[child.tag]
             else:
-                if child.tag not in result:  # 因为object可能有多个，所以需要放入列表里
+                if child.tag not in result:
                     result[child.tag] = []
                 result[child.tag].append(child_result[child.tag])
         return {xml.tag: result}
 
-    def coco_index(self, idx):
+    def coco_index(self, idx: int) -> dict:
         """
         该方法是专门为pycocotools统计标签信息准备，不对图像和标签作任何处理
         由于不用去读取图片，可大幅缩减统计时间
